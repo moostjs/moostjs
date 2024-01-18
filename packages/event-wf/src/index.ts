c818dd6d2b0b486ce43417ab6b8f62f052680706
mav-rik <mav777art@gmail.com>
2024-01-18 22:31:07 +0100

    feat(wf): export useWfState for imports

--- packages/event-wf/src/index.ts
+++ packages/event-wf/src/index.ts
@@ -1,4 +1,4 @@
 export * from './event-wf'
 export * from './decorators'
-export { useWFContext } from '@wooksjs/event-wf'
+export { useWFContext, useWfState } from '@wooksjs/event-wf'
 export { TFlowOutput, TWorkflowSchema, StepRetriableError } from '@prostojs/wf'
