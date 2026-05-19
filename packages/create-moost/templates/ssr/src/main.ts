8448d4b7455c7a07810b4e99a7c11c32bb6bb09e
mav-rik <mav777art@gmail.com>
2026-05-20 01:25:22 +0200

    fix(create-moost): await listen/init and register controllers first in SSR template

--- packages/create-moost/templates/ssr/src/main.ts
+++ packages/create-moost/templates/ssr/src/main.ts
@@ -3,6 +3,6 @@ import { MoostHttp } from '@moostjs/event-http'
 import { ApiController } from './controllers/api.controller'
 
 const app = new Moost()
-app.adapter(new MoostHttp()).listen(3000)
 app.registerControllers(ApiController)
-app.init()
+await app.adapter(new MoostHttp()).listen(3000)
+await app.init()
